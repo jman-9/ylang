0acc5f3864d4693869c09a068dfcb9e25676319a
jman <flute999@gmail.com>
2025-12-01 11:49:31 +0900

    Add 'floor()' to math

--- core/builtin/Math.cpp
+++ core/builtin/Math.cpp
@@ -55,6 +55,18 @@ YRet Sqrt(YArgs* args)
 	return yr;
 }
 
+YRet Floor(YArgs* args)
+{
+	auto a1 = (Variable*)args->args[0].o;
+	double x = *a1 == Variable::INT ? (double)a1->_int : a1->_float;
+	double v = floor(x);
+
+	YRet yr;
+	yr.single.o = Variable::NewFloat(v);
+	yr.single.tp = YEArg::YVar;
+	return yr;
+}
+
 const ymod::ModuleDesc& GetModuleDesc()
 {
 	static ymod::ModuleDesc m;
@@ -67,6 +79,7 @@ const ymod::ModuleDesc& GetModuleDesc()
 		m.memberTbl[ "cos" ] = { "cos", ymod::ModuleMemberDesc::FUNC, false, 1, Cos};
 		m.memberTbl[ "tan" ] = { "tan", ymod::ModuleMemberDesc::FUNC, false, 1, Tan};
 		m.memberTbl[ "sqrt" ] = { "sqrt", ymod::ModuleMemberDesc::FUNC, false, 1, Sqrt};
+		m.memberTbl[ "floor" ] = { "floor", ymod::ModuleMemberDesc::FUNC, false, 1, Floor};
 	}
 	return m;
 }
