e8f14380f5d3f4ca2b07b569d760936c02fdcf1b
jman <flute999@gmail.com>
2025-12-11 16:57:05 +0900

    Fix for linux build

--- core/builtin/Rand.cpp
+++ core/builtin/Rand.cpp
@@ -9,6 +9,7 @@
 	#define RANDOMD(__min__, __max__) ((((double)((rand()<<15) | (rand()&0) | rand())) / ((RAND_MAX<<15 | RAND_MAX) + 1) * ((((double)__max__)) - (double)(__min__))) + (double)(__min__))
 #else
 	#define RANDOM(__min__, __max__) ((int64_t)rand() * ((__max__) + 1 - (__min__)) / ((int64_t)RAND_MAX + 1) + (__min__))
+	#define RANDOMD(__min__, __max__) ((double)rand() / ((int64_t)RAND_MAX + 1) *  (((double)(__max__)) - ((double)(__min__)))+ (double)(__min__))
 #endif
 
 
